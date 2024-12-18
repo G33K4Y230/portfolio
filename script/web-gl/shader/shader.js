22ca89512e97bf1b36b784a2a81074b136934792
gee <gkandandji@gmail.com>
2024-12-18 12:42:33 +0000

    refactor shader

--- script/web-gl/shader/shader.js
+++ script/web-gl/shader/shader.js
@@ -1,7 +1,7 @@
 import { gl } from "./../canvas.js"
 import { printFile, readFile } from "./../util/file.js"
 
-let program;
+export let SHADER_DEFAULT;
 
 const generateGLShader = (contents, type) => {
     const shader = gl.createShader(type == "vertex" ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER);
@@ -15,66 +15,73 @@ const generateGLShader = (contents, type) => {
     return shader;
 }
 
-const generateShader 
-
-export async function generateProgram() {
-
-    const vertexShaderName = "default";
-    const fragmentShaderName = "default";
-    
-    const vertexPath = "script/web-gl/shader/vertex/" + vertexShaderName + ".shader";
-    const vertexContents = await readFile(vertexPath);
-    const vertexShader = generateGLShader(vertexContents, "vertex");
-
-    const fragmentPath = "script/web-gl/shader/fragment/" + fragmentShaderName + ".shader"
-    const fragmentContents = await readFile(fragmentPath);
-    const fragmentShader = generateGLShader(fragmentContents, "fragment");
-
-    program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    if(!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-	console.error("Error! Failed to link shader program!\n", glGetProgramInfoLog(program));
-	return;
-    }
-    
-    var positionAttribLocation = gl.getAttribLocation(program, "aPosition");
-    gl.vertexAttribPointer(
-	positionAttribLocation,
-	3,
-	gl.FLOAT,
-	gl.FALSE,
-	5 * Float32Array.BYTES_PER_ELEMENT,
-	0
-    );
-    var uvAttribLocation = gl.getAttribLocation(program, "aUV");
-    gl.vertexAttribPointer(
-	uvAttribLocation,
-	2,
-	gl.FLOAT,
-	gl.FALSE,
-	5 * Float32Array.BYTES_PER_ELEMENT,
-	3 * Float32Array.BYTES_PER_ELEMENT
-    );
-
-    gl.enableVertexAttribArray(positionAttribLocation);
-    gl.enableVertexAttribArray(uvAttribLocation);
-
-    gl.useProgram(program);
+export async function generateShaders() {
+    SHADER_DEFAULT = new Shader("default", "default");
 }
 
 class Shader {
 
     constructor(vertexShaderName, fragmentShaderName) {
-	const vertexPath = "script/web-gl/shader/vertex/" + vertexShaderName + ".shader";
-    const vertexContents = await readFile(vertexPath);
-    const vertexShader = generateGLShader(vertexContents, "vertex");
+	this._vertexShaderName = vertexShaderName;
+	this._fragmentShaderName = fragmentShaderName;
+	this._program = null;
+	this.generate();
+    }
 
-    const fragmentPath = "script/web-gl/shader/fragment/" + fragmentShaderName + ".shader"
-    const fragmentContents = await readFile(fragmentPath);
-    const fragmentShader = generateGLShader(fragmentContents, "fragment");
+    get program() {
+	return this._program;
+    }
 
+    async generate() {
+
+	const vertexPath = "script/web-gl/shader/vertex/" + this._vertexShaderName + ".shader";
+	const vertexContents = await readFile(vertexPath);
+	const vertexShader = generateGLShader(vertexContents, "vertex");
+
+	const fragmentPath = "script/web-gl/shader/fragment/" + this._fragmentShaderName + ".shader"
+	const fragmentContents = await readFile(fragmentPath);
+	const fragmentShader = generateGLShader(fragmentContents, "fragment");
+
+	this._program = gl.createProgram();
+	gl.attachShader(this._program, vertexShader);
+	gl.attachShader(this._program, fragmentShader);
+	gl.linkProgram(this._program);
+	if(!gl.getProgramParameter(this._program, gl.LINK_STATUS)) {
+	    console.error("Error! Failed to link shader program!\n", glGetProgramInfoLog(program));
+	    return;
+	}
+	
+	this.generateProgramAttributes();	
+    }
+
+    generateProgramAttributes() {
+
+	var positionAttribLocation = gl.getAttribLocation(this._program, "aPosition");
+	gl.vertexAttribPointer(
+	    positionAttribLocation,
+	    3,
+	    gl.FLOAT,
+	    gl.FALSE,
+	    5 * Float32Array.BYTES_PER_ELEMENT,
+	    0
+	);
+	
+	var uvAttribLocation = gl.getAttribLocation(this._program, "aUV");
+	gl.vertexAttribPointer(
+	    uvAttribLocation,
+	    2,
+	    gl.FLOAT,
+	    gl.FALSE,
+	    5 * Float32Array.BYTES_PER_ELEMENT,
+	    3 * Float32Array.BYTES_PER_ELEMENT
+	);
+
+	gl.enableVertexAttribArray(positionAttribLocation);
+	gl.enableVertexAttribArray(uvAttribLocation);
+    }
+
+    use() {
+	gl.useProgram(this._program);
     }
     
 }
